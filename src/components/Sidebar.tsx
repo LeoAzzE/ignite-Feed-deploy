7d5ae01b4d3f447f63667d8b26fec9912c44a4b6
Borges dev <leosull50@hotmail.com>
2023-02-27 19:18:46 -0300

    more change

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={styles.profile}>
-                <Avatar src="https://github.com/maykbrito.png" />
+                <Avatar src="https://github.com/LeoAzzE.png" />
                 <strong>Leo Chaves</strong>
                 <span>Web Developer</span>
             </div>
